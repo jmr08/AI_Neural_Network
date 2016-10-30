82b6379383089801a9e6957ec1181d643cb3e06b
jmr08 <jenriquemendozar@gmail.com>
2016-10-30 18:34:10 -0500

    init project.

---
+++ NeuronalFunctionAND/src/ups/edu/ec/main/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ups.edu.ec.main;
+
+/**
+ *
+ * @author Sir Jorge Mendoza
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
